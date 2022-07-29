935322350e91c44af16829ae9b24a48aacf8241b
Ryu-1111 <killa.ikazuti@icloud.com>
2022-07-30 00:50:49 +0900

    add new file

---
+++ a.js
@@ -0,0 +1,35 @@
+(() => {
+
+    const $elm = document.querySelector('#js-accordion');
+    const $trigger = $elm.getElementsByTagName('a');
+
+    const triggerLen = $trigger.length;
+    let index = 0;
+    while (index < triggerLen) {
+
+        $trigger[index].addEventListener('click', (e) => clickHndler(e));
+        index++;
+    }
+
+
+
+    const clickHndler = (e) => {
+        e.preventDefault();
+
+        const $target = e.currentTarget;
+
+        const $content = $target.nextElementSibling;
+        if ($content.style.display === 'block') {
+            $content.style.display = 'none';
+        } else {
+            $content.style.display = 'block'
+        }
+    }
+
+    console.log('Clicked!');
+
+
+
+
+
+})();
